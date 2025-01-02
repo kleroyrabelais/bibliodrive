2b702b2b4235e999faa5de797d30e3bdcbf5552d
kleroyrabelais <kleroypro22@gmail.com>
2025-01-02 11:12:06 +0100

    redirection vers index.php

--- detail.php
+++ detail.php
@@ -44,7 +44,7 @@ if (isset($_GET["Auteur"]) && !empty($_GET["Auteur"])) {
             echo "</div><hr>";
         }
     }
-    
+}    
 ?>
 
 
--- navbar.php
+++ navbar.php
@@ -5,7 +5,7 @@
         crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Page d'acceuil (maintenance jusqu'au 6 janvier 2025)</a>
+      <a class="navbar-brand" href="index.php">Page d'acceuil (maintenance jusqu'au 6 janvier 2025)</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
       aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
